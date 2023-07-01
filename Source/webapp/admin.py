ffe47585fc5c0eaa6b4e5c3f5d5f9a979f12a516
Chyngyz <konuratov.chika@gmail.com>
2023-07-01 12:19:02 +0600

    Создал админку и сделал миграцию

--- Source/webapp/admin.py
+++ Source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import GuestBook
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'email', 'status', 'created_at']
+    list_display_links = ['id', 'title']
+    list_filter = ['status']
+    search_fields = ['title', 'text', 'email']
+    fields = ['title', 'email', 'text', 'status']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
